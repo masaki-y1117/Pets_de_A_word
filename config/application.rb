27664b57f7dbe6e9cbc1af39da8ba598c74f0a5d
masaki-y1117 <my1117yamamoto@gmail.com>
2021-03-06 22:02:10 +0000

    [Add]gem追加

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module PetDeAWord
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.generators.template_engine = :slim  
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
